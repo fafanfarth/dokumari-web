a81047c66096435911ff10518bd2274bcba867c9
fafanfarth <92444047+fafanfarth@users.noreply.github.com>
2026-09-19 12:13:07 +0900

    Web build 2026-09-19 12:13

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"06a2e2a110089dff50fe635cffd2a61e1b24fb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "774188897" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "553677590" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
